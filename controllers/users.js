7f7e0f7ab30916e9f3d52d76f87cad92c71a1d82
Данила <suhorukovdanila@yandex.ru>
2022-03-16 21:51:16 +0300

    Add files via upload

---
+++ controllers/users.js
@@ -0,0 +1,75 @@
+const User = require('../model/user');
+const { ERROR_CODE_400, ERROR_CODE_404, ERROR_CODE_500 } = require('../errors/errorsCode');
+
+module.exports.getUsers = (req, res) => {
+    User.find({})
+        .then((users) => res.send(users))
+        .catch((err) => res.status(ERROR_CODE_500).send({ message: 'Ошибка по умолчанию.', ...err }));
+};
+
+module.exports.getUserById = (req, res) => {
+    User.findById(req.params.userId)
+        .then((user) => {
+            if (user) {
+                res.status(200).send(user);
+            } else {
+                res.status(ERROR_CODE_404).send({ message: 'Пользователь не найден' });
+            }
+        })
+        .catch((err) => {
+            if (err.name === 'ValidationError' || err.name === 'CastError') {
+                return res.status(ERROR_CODE_400).send({ message: 'Проверьте введенные данные' });
+            }
+            return res.status(ERROR_CODE_500).send({ message: 'Ошибка по умолчанию.', ...err });
+        });
+};
+
+module.exports.createUser = (req, res) => {
+    const { name, about, avatar } = req.body;
+
+    User.create({ name, about, avatar })
+        .then((user) => res.send({ data: user }))
+        .catch((err) => {
+            if (err.name === 'ValidationError') {
+                return res.status(ERROR_CODE_400).send({ message: 'Проверьте введенные данные' });
+            }
+            return res.status(ERROR_CODE_500).send({ message: 'Ошибка по умолчанию.', ...err });
+        });
+};
+
+module.exports.updateUserInfo = (req, res) => {
+    const { name, about } = req.body;
+
+    User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
+        .then((user) => {
+            if (user) {
+                return res.status(200).send({ data: user });
+            }
+            return res.status(ERROR_CODE_404).send({ message: 'Пользователь не найден' });
+        })
+        .catch((err) => {
+            if (err.name === 'ValidationError' || err.name === 'CastError') {
+                return res.status(ERROR_CODE_400).send({ message: 'Проверьте введенные данные' });
+            }
+            return res.status(ERROR_CODE_500).send({ message: 'Ошибка по умолчанию.', ...err });
+        });
+};
+
+module.exports.updateAvatar = (req, res) => {
+    const { avatar } = req.body;
+
+    User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
+        .then((user) => {
+            if (user) {
+                res.status(200).send({ data: user });
+            } else {
+                res.status(ERROR_CODE_404).send({ message: 'Пользователь не найден' });
+            }
+        })
+        .catch((err) => {
+            if (err.name === 'ValidationError' || err.name === 'CastError') {
+                return res.status(ERROR_CODE_400).send({ message: 'Проверьте введенные данные' });
+            }
+            return res.status(ERROR_CODE_500).send({ message: 'Ошибка по умолчанию.', ...err });
+        });
+};
